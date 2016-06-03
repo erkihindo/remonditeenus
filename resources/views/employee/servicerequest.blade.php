67f23940b29789b7e5e08c587c93f40221b22b78
erki <erkihindo@gmail.com>
2016-06-03 23:27:39 +0300

    Tegin kliendi pöördumise vormi

--- resources/views/employee/servicerequest.blade.php
+++ resources/views/employee/servicerequest.blade.php
@@ -1,23 +1,34 @@
 @extends('layouts.employeeapp')
 
 @section('content')
+
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Enter the request</div>
-
-                <div class="panel-body">
-                    <table>
-                        <tbody>
-                            <tr>
-                             enter form here   
-                            </tr>
-                        </tbody>
-                        
-                    </table>
-                </div>
-            </div>
+            <form action="{{route('servicerequest')}}" method="POST" style="margin-left: 14%">
+                <input type="hidden" value="{{ Session::token() }}" name="_token">
+                <table>
+                    <tr>
+                        <td colspan="2">Kliendi pöördumine nr </td>
+                    </tr>
+                    <tr>
+                        <td>Klient:</td>
+                        <td><input type="button" value="Otsi klienti"></td>
+                    </tr>
+                    <tr>
+                        <td>Kliendi kirjeldus:</td>
+                        <td><textarea></textarea></td>
+                    </tr>
+                    <tr>
+                        <td>Vastuvõtja kirjeldus:</td>
+                        <td><textarea></textarea></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" value="Salvesta kliendi pöördumine"></td>
+                        <td><input type="button" value="Vormista tellimus"></td>
+                    </tr>
+                </table>
+            </form>
         </div>
     </div>
 </div>
