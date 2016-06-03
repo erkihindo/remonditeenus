be508f382c1e67add8a32d3753a0162b2c2589d7
erki <erkihindo@gmail.com>
2016-06-03 15:39:09 +0300

    added models

---
+++ app/Service_order.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Service_order extends Model
+{
+    //
+}
---
+++ app/Service_request_status_type.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Service_request_status_type extends Model
+{
+    //
+}
