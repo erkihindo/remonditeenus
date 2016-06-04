489949690c0cdd72206a195a587df107da57992c
erki <erkihindo@gmail.com>
2016-06-04 15:41:59 +0300

    tellimuse vorm

--- resources/views/employee/serviceorder.blade.php
+++ resources/views/employee/serviceorder.blade.php
@@ -5,20 +5,42 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">New order nr {{ $newID }}</div>
+            <form action="{{route('serviceorder.create')}}" method="POST" style="margin-left: 14%">
+                <input type="hidden" value="{{ Session::token() }}" name="_token">
+                <table>
+                    <tr>
+                        <td colspan="2">Tellimus nr {{ $newID }}</td>
+                    </tr>
+                    <tr>
+                        <td>Vali tellimuse seade:</td>
+                        <td><select name="device"></select></td>
+                    </tr>
+                    <tr>
+                        <td>Summa kokku:</td>
+                        <td>{{ $total_order_price }}</td>
+                    </tr>
+                    <tr>
+                        <td>Töö: <input type="text" name="job"></td>
+                        <td>Teenus: <select name="service"></select></td>
+                        <td>kogus: <input type="number" name="amount1"></td>
+                        <td>ühiku hind: <input type="number" name="unit_price1"> [h]</td>
+                        <td>hind kokku:<input type="number" name="total_price1"</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Osa: <input type="text" name="part"></textarea></td>
+                        <td>kogus: <input type="number" name="amount2"> [tk]</td>
+                        <td>ühiku hind: <input type="number" name="unit_price2"></td>
+                        <td>hind kokku: <input type="number" name="total_price2"</td>
+                    </tr>
 
-                <div class="panel-body">
-                    <table>
-                        <tbody>
-                            <tr>
-                             Enter order form here
-                            </tr>
-                        </tbody>
-                        
-                    </table>
-                </div>
-            </div>
+                    <tr>
+                        <td>tellimuse staatus:</td>
+                        <td><select name="order_status"></select></td>
+                        <td><input type="button" value="Salvesta tellimus"></td>
+                        <td><input type="submit" value="Tee arve"></td>
+                    </tr>
+                </table>
+            </form>
         </div>
     </div>
 </div>
